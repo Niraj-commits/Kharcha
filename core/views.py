ccb8ee716c762c4de191d352f81ea16f844e03c1
Niraj-commits <karki.ni018@gmail.com>
2025-04-21 03:00:05 +0000

    added hosted site and new styles

--- core/views.py
+++ core/views.py
@@ -29,15 +29,23 @@ def CustomLogin(request):
 
     return render(request, 'authentication/login.html')
 
+from django.contrib.auth.hashers import make_password
+from django.contrib import messages
+from django.shortcuts import redirect, render
+
 def Register(request):
     if request.method == "POST":
         username = request.POST.get("username")
         email = request.POST.get('email')
         address = request.POST.get('address')
         password = request.POST.get('password')
-        hashed_password = make_password(password)
-        
-        User.objects.create(username = username,email=email,address = address,password = hashed_password)
-        
-        return redirect("login")
-    return render(request,"authentication/register.html")
+
+        if len(password) > 8:
+            hashed_password = make_password(password)
+            User.objects.create(username=username, email=email, address=address, password=hashed_password)
+            messages.success(request, "Registration successful")
+            return redirect("login")
+        else:
+            messages.error(request, "Password length cannot be less than 8")
+            return redirect('register')
+    return render(request, "authentication/register.html")
